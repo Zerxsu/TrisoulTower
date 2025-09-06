d65c4f18db98b5ff8de8a218e93318532ff8dc72
HoppXR <brtl.hopper@gmail.com>
2025-09-05 21:39:29 -0400

    character and camera movement

---
+++ Source/TrisoulTower/CharacterController.cpp
@@ -0,0 +1,64 @@
+#include "CharacterController.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+ACharacterController::ACharacterController()
+{
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+
+	GetMesh()->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, -90.0f), FQuat(FRotator(0.0f, -90.0f, 0.0f)));
+
+	SpringArmComp->SetupAttachment(RootComponent);
+	CameraComp->SetupAttachment(SpringArmComp, USpringArmComponent::SocketName);
+
+	SpringArmComp->bUsePawnControlRotation = true;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->bUseControllerDesiredRotation = true;
+	GetCharacterMovement()->bIgnoreBaseRotation = true;
+}
+
+void ACharacterController::BeginPlay()
+{
+	Super::BeginPlay();
+	if (APlayerController* PC = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PC->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(InputContext, 0);
+		}
+	}
+}
+
+void ACharacterController::Move(const FInputActionValue& Value)
+{
+	const FVector2D MovementValue = Value.Get<FVector2D>();
+
+	if (Controller)
+	{
+		AddMovementInput(GetActorRightVector(), MovementValue.X);
+		AddMovementInput(GetActorForwardVector(), MovementValue.Y);
+	}
+}
+
+void ACharacterController::Look(const FInputActionValue& Value)
+{
+	const FVector2D LookValue = Value.Get<FVector2D>();
+
+	if (Controller)
+	{
+		AddControllerYawInput(LookValue.X);
+		AddControllerPitchInput(LookValue.Y);
+	}
+}
+
+void ACharacterController::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	if (UEnhancedInputComponent* EnhancedInputComp = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComp->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ACharacterController::Move);
+		EnhancedInputComp->BindAction(LookAction, ETriggerEvent::Triggered, this, &ACharacterController::Look);
+	}
+}
---
+++ Source/TrisoulTower/CharacterController.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "CharacterController.generated.h"
+
+
+UCLASS()
+class TRISOULTOWER_API ACharacterController : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ACharacterController();
+
+	virtual void SetupPlayerInputComponent(UInputComponent* InputComponent) override;
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	class USpringArmComponent* SpringArmComp;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	class UCameraComponent* CameraComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UInputMappingContext* InputContext;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UInputAction* LookAction;
+
+	void BeginPlay() override;
+	
+	UFUNCTION()
+	void Move(const FInputActionValue& Value);
+
+	UFUNCTION()
+	void Look(const FInputActionValue& Value);
+};
