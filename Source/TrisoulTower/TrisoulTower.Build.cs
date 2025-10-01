ca0353e32a750d98f2fa22ecd6f8846b4ddcd65b
HoppXR <brtl.hopper@gmail.com>
2025-10-01 00:20:22 -0400

    Audio Manager created
    Features added:
    - play sound effect
    - play music
    - change states (eg. normal, combat)
    - change parameter values

--- Source/TrisoulTower/TrisoulTower.Build.cs
+++ Source/TrisoulTower/TrisoulTower.Build.cs
@@ -8,7 +8,7 @@ public class TrisoulTower : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AkAudio", "WwiseSoundEngine" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
