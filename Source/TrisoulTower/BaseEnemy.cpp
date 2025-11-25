e3384b6ec5f483c39f27aa7836fdae509c18d3f7
RyanSteele <ryannicolassteele@gmail.com>
2025-11-25 15:37:26 -0500

    ryan stuff works again

--- Source/TrisoulTower/BaseEnemy.cpp
+++ Source/TrisoulTower/BaseEnemy.cpp
@@ -1,12 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "BaseEnemy.h"
 #include "Containers/UnrealString.h"
 #include "GameFramework/PawnMovementComponent.h"
 #include "NavigationPath.h"
 #include "Kismet/GameplayStatics.h"
 #include "Kismet/KismetSystemLibrary.h"
-#include "BaseEnemy.h"
+
 
 // Sets default values
 ABaseEnemy::ABaseEnemy()
--- Source/TrisoulTower/TrisoulTower.Build.cs
+++ Source/TrisoulTower/TrisoulTower.Build.cs
@@ -12,6 +12,10 @@ public class TrisoulTower : ModuleRules
 		
 		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTasks", "GameplayTags" });
 
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule" });
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem" });
+
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
